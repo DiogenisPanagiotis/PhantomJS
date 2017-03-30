110cce932bcf4b0c9504d8b28e9c3851b1707561
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2017-03-30 11:23:17 -0400

    Add functionality to screencapture func.

--- screencapture.js
+++ screencapture.js
@@ -1,7 +1,13 @@
 "use strict";
 
+// Loads url (https://github.com/) and saves it as an image in the same directory
+// the script was run in.
+
 var page = require('webpage').create(); // create web page object
-page.open('http://github.com/', function() {
-  page.render('github.png'); // save screenshot of web page
+page.open('http://github.com/', function(status) {
+  console.log("Status: ", status);
+  if (status === "success") {
+    page.render('github.png'); // save screenshot of web page
+  }
   phantom.exit(); // terminate
 });
